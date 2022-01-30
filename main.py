6a1cf656e9eed43d20e27607c3639a8b25c3e9ad
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-01-30 15:33:25 +0100

    added support for 2x16 i2c lcd display

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
-import utime
+import time
 import machine
-import aht20
+from lib import aht20
+from lib import pico_i2c_lcd
+
+
+def scan_i2c_devices(i2c):
+    devices = i2c.scan()
+
+    for device in devices:
+        print(f"{device} | address {hex(device)}")
+
+
+def print_lcd(display, temp, humidity):
+    display.clear()
+
+    display.move_to(1, 0)
+    current_time = time.gmtime()
+    display.putstr(f"{current_time[3]}:{current_time[4]}:{current_time[5]}")
+
+    display.move_to(1, 1)
+    display.putstr(f"T:{round(temp, 1)}C  ")
+    display.putstr(f"H:{int(humidity)}%")
 
 
 def main():
     onboard_led = machine.Pin(25, machine.Pin.OUT)
     onboard_led.on()
 
-    i2c = machine.I2C(0, scl=machine.Pin(9), sda=machine.Pin(8), freq=400000)
-    # print(hex(i2c.scan()[0]))
+    i2c = machine.I2C(0, scl=machine.Pin(9), sda=machine.Pin(8), freq=100000)
+
+    scan_i2c_devices(i2c)
 
     sensor = aht20.AHT20(i2c)
+    display = pico_i2c_lcd.I2cLcd(i2c, 2, 16)
 
     while True:
-        print(f"Temperature: {sensor.temperature()} C")
-        print(f"Humidity: {sensor.relative_humidity()}")
+        print_lcd(display, sensor.temperature(), sensor.relative_humidity())
 
-        utime.sleep(1)
+        time.sleep(1)
 
 
 if __name__ == '__main__':
