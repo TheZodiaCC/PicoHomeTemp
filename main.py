d5e9cfeb1f60693bb4a4b3d3984dc6a2f2ac39f3
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-01-31 20:47:07 +0100

    extracted station logic to seperated class + added i2c devices init checks

--- main.py
+++ main.py
@@ -1,32 +1,10 @@
-import time
-import machine
-from lib import aht20
-from lib import pico_i2c_lcd
-
-
-def print_lcd(display, temp, humidity):
-    display.clear()
-
-    display.move_to(0, 0)
-    display.putstr(f"Temp: {round(temp, 2)}C")
-
-    display.move_to(0, 1)
-    display.putstr(f"Humidity: {round(humidity, 2)}%")
+from station import Station
 
 
 def main():
-    onboard_led = machine.Pin(25, machine.Pin.OUT)
-    onboard_led.on()
+    station = Station()
 
-    i2c = machine.I2C(0, scl=machine.Pin(9), sda=machine.Pin(8), freq=100000)
-
-    sensor = aht20.AHT20(i2c)
-    display = pico_i2c_lcd.I2cLcd(i2c, 2, 16)
-
-    while True:
-        print_lcd(display, sensor.temperature(), sensor.relative_humidity())
-
-        time.sleep(3)
+    station.run()
 
 
 if __name__ == '__main__':
---
+++ station.py
@@ -0,0 +1,48 @@
+import time
+import machine
+from lib import aht20
+from lib import pico_i2c_lcd
+
+
+class Station:
+    def __init__(self):
+        self.onboard_led = machine.Pin(25, machine.Pin.OUT)
+        self.i2c = machine.I2C(0, scl=machine.Pin(9), sda=machine.Pin(8), freq=100000)
+
+        self.temp_sensor = None
+        self.display = None
+
+        self.initialized = False
+
+    def init_sensor(self):
+        try:
+            self.temp_sensor = aht20.AHT20(self.i2c)
+            self.display = pico_i2c_lcd.I2cLcd(self.i2c, 2, 16)
+
+            self.initialized = True
+
+        except Exception as e:
+            self.initialized = False
+
+    def print_lcd(self, display, temp, humidity):
+        display.clear()
+
+        display.move_to(0, 0)
+        display.putstr(f"Temp: {round(temp, 2)}C")
+
+        display.move_to(0, 1)
+        display.putstr(f"Humidity: {round(humidity, 2)}%")
+
+    def process(self):
+        while True:
+            if self.initialized:
+                self.print_lcd(self.display, self.temp_sensor.temperature(), self.temp_sensor.relative_humidity())
+
+            else:
+                self.onboard_led.toggle()
+
+            time.sleep(3)
+
+    def run(self):
+        self.init_sensor()
+        self.process()
